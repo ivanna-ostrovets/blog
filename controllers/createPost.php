766190e3a082d5f9cae747ebbc18051cba1dceb8
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-02-08 03:59:31 +0200

    Finish functionality for creating posts.

--- controllers/createPost.php
+++ controllers/createPost.php
@@ -1,11 +1,50 @@
 <?php
-require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/db/database.php');
 require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/services/postService.php');
+require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/scripts/thumbnailCreate.php');
 
 $title = $_POST['title'];
 $teaser = $_POST['teaser'];
 $content = $_POST['content'];
-$miniature = $_POST['miniature'];
 $category = $_POST['category'];
 
-$postService->createPost($title, $category, $teaser, $content, $miniature);
+if (!empty($_FILES['image']['tmp_name'])) {
+  $imagePath = '/public/posts_images/' . basename($_FILES["image"]["name"]);
+  $imageFileType = pathinfo($imagePath, PATHINFO_EXTENSION);
+
+  $errors = array();
+
+  if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+    && $imageFileType != "gif"
+  ) {
+    $errors[] = array("message" => "Sorry, only JPG, JPEG, PNG & GIF files are allowed.");
+  }
+
+  if (getimagesize($_FILES["image"]["tmp_name"]) === FALSE) {
+    $errors[] = array("message" => "File is not an image.");
+  }
+
+  if (!move_uploaded_file($_FILES["image"]["tmp_name"], realpath($_SERVER["DOCUMENT_ROOT"]) . $imagePath)) {
+    $errors[] = array("message" => "Sorry, there was an error uploading your file.");
+  } else {
+    thumbnailCreate(realpath($_SERVER["DOCUMENT_ROOT"]) . $imagePath);
+  }
+} else {
+  $imagePath = "";
+}
+
+if (empty($category)) {
+  $errors[] = array("message" => "Please, select category.");
+}
+
+if (empty($errors)) {
+  try {
+    $postService->createPost($title, $category, $teaser, $content, $imagePath);
+  } catch (Exception $e) {
+    $errors[] = array("message" => $e->getMessage());
+  }
+}
+
+if (!empty($errors)) {
+  echo json_encode(array('errors' => $errors));
+  http_response_code(400);
+}
--- views/createPost.php
+++ views/createPost.php
@@ -1,5 +1,6 @@
 <?php
-require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/db/database.php');
+session_start();
+
 require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/services/userService.php');
 require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/services/categoryService.php');
 
@@ -8,56 +9,76 @@ $categories = $categoryService->getAllCategories();
 
 <!DOCTYPE html>
 <html>
-<head>s
+<head>
   <?php include realpath($_SERVER["DOCUMENT_ROOT"]) . '/partials/includes.php'; ?>
   <script src="//cdn.ckeditor.com/4.5.5/standard/ckeditor.js"></script>
+  <script src="../public/js/ajaxSubmitCreatePostForm.js"></script>
 </head>
 <body>
 <?php include realpath($_SERVER["DOCUMENT_ROOT"]) . '/partials/nav.php'; ?>
 
 <main class="main">
-  <form action="../controllers/createPost.php" method="post" id="create_post_form">
+  <div class="errors"
+       id="errors">
+  </div>
+
+  <form onsubmit="return ajaxSubmitCreatePostForm(this)"
+        method="post"
+        id="create_post_form"
+        enctype="multipart/form-data">
     <div class="form-group">
-      <label>Title</label>
-      <input type="text" name="title" id="title" placeholder="Enter title">
+      <label for="title">Title</label>
+      <input type="text"
+             name="title"
+             id="title"
+             placeholder="Enter title"
+             required>
     </div>
 
     <div class="form-group">
-      <label>Miniature</label>
-      <input type="file" name="miniature" id="miniature">
+      <label for="image">Image</label>
+      <input type="file"
+             name="image"
+             id="image"
+             accept="image/*">
     </div>
 
     <div class="form-group">
-      <label>Category</label>
-      <select name="category" form="create_post_form">
+      <label for="category">Category</label>
+      <select name="category" id="category">
+        <option value="" disabled selected>- Select one -</option>
         <?php foreach ($categories as $key => $value): ?>
-          <option value="<?= $key ?>">
+          <option value="<?= $key + 1 ?>">
             <?= $value['category'] ?>
-          </option>;
+          </option>
         <?php endforeach; ?>
       </select>
     </div>
 
     <div class="form-group">
-      <label>Content</label>
-      <textarea class="ckeditor" id="content" name="content">
+      <label for="content">Content</label>
+      <textarea class="ckeditor"
+                id="content"
+                name="content"
+                required>
+      </textarea>
       <script>
         CKEDITOR.replace('content');
       </script>
-      </textarea>
     </div>
 
     <div class="form-group">
-      <label>Teaser</label>
-      <textarea id="teaser" name="teaser"></textarea>
+      <label for="teaser">Teaser</label>
+      <textarea id="teaser"
+                name="teaser"
+                required>
+      </textarea>
     </div>
 
-    <input type="submit" value="Submit">
+    <button type="submit">Submit</button>
+
     <a href="../index.php">Cancel</a>
   </form>
 </main>
-
-<footer>
-</footer>
 </body>
 </html>
