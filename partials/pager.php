94f4760347f18dc8660a7e8408a4ecadfa4b3541
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-02-09 00:55:10 +0200

    Add pagination styles.

--- partials/pager.php
+++ partials/pager.php
@@ -1,13 +1,52 @@
-<div class="pager">
-  <?php for ($i = 0; $i < $postService->postCount(); $i += 10):
+<nav class="text-center">
+  <ul class="pagination">
+    <?php
     $urlPart = "";
     if (isset($_GET['category'])) {
       $urlPart = "&category={$_GET['category']}";
-    } ?>
+    }
 
-    <a href="index.php?offset=<?= $i . $urlPart?>" class="pages">
-      <?= $i + 1 ?> - <?= $i + 10 ?>
-    </a>
+    $postsNumber = $postService->postCount();
+    ?>
 
-  <?php endfor; ?>
-</div>
+    <li>
+      <?php if ($_SERVER['REQUEST_URI'] === '/index.php' || (int) $_GET['offset'] === 0): ?>
+        <a href="index.php?offset=<?= $postsNumber - 10 . $urlPart ?>">
+      <?php elseif (isset($_GET['offset'])): ?>
+        <a href="index.php?offset=<?= (int) $_GET['offset'] - 10 . $urlPart ?>">
+      <?php endif; ?>
+          <span>&laquo;</span>
+        </a>
+    </li>
+
+    <?php for ($i = 0; $i < $postsNumber; $i += 10): ?>
+      <li
+        <?php if (isset($_GET['offset'])): ?>
+          <?php if ((int) $_GET['offset'] === $i): ?>
+            class="active"
+          <?php endif; ?>
+        <?php elseif ($_SERVER['REQUEST_URI'] === '/index.php'): ?>
+          <?php if ($i === 0): ?>
+            class="active"
+          <?php endif; ?>
+        <?php endif; ?>
+      >
+        <a href="index.php?offset=<?= $i . $urlPart ?>">
+          <?= $i / 10 + 1 ?>
+        </a>
+      </li>
+    <?php endfor; ?>
+
+    <li>
+      <?php if (isset($_GET['offset']) && (int) $_GET['offset'] === $postsNumber - 10 && (int) $_GET['offset'] < 100): ?>
+        <a href="index.php?offset=0<?= $urlPart ?>">
+      <?php elseif (isset($_GET['offset'])): ?>
+        <a href="index.php?offset=<?= (int) $_GET['offset'] + 10 . $urlPart ?>">
+      <?php elseif ($_SERVER['REQUEST_URI'] === '/index.php'): ?>
+        <a href="index.php?offset=10<?= $urlPart ?>">
+      <?php endif; ?>
+          <span>&raquo;</span>
+        </a>
+    </li>
+  </ul>
+</nav>
