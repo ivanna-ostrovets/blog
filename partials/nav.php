8af263dcb35cae5fd02d592f97f4f4261b965e56
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-02-09 18:43:51 +0200

    Fix bug with hamburger menu.

--- partials/nav.php
+++ partials/nav.php
@@ -3,12 +3,36 @@ require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/services/categoryService.ph
 require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/services/userService.php');
 
 $categories = $categoryService->getAllCategories();
-$url = substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], "?"));
+$currentCategory = $categoryService->getCurrentCategory()['category'];
+
+$urlPart = "";
+$urlPartOffset = "";
+
+if (isset($_GET['category']) && isset($_GET['offset'])) {
+  $urlPart = "?category={$_GET['category']}&offset={$_GET['offset']}";
+} else {
+  if (isset($_GET['offset'])) {
+    $urlPart = "?offset={$_GET['offset']}";
+    $urlPartOffset = "?offset={$_GET['offset']}";
+  } else {
+    if (isset($_GET['category'])) {
+      $urlPart = "?category={$_GET['category']}";
+    }
+  }
+}
+
+$url = $_SERVER['REQUEST_URI'];
 ?>
 
 <nav class="navbar navbar-inverse navbar-fixed-top">
   <div class="container-fluid">
     <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
       <a class="navbar-brand"
          href="/index.php">
         Blog
@@ -17,12 +41,12 @@ $url = substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], "?"));
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active">
-          <a href="/index.php" class="
-          <?php if ($url == '/index.php'): ?>
-              active
+        <li class="
+          <?php if ($url == "/index.php" . $urlPartOffset): ?>
+            active
           <?php endif; ?>
-          ">Home</a>
+        ">
+          <a href="/index.php">Home</a>
         </li>
 
         <li class="dropdown">
@@ -46,15 +70,27 @@ $url = substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], "?"));
         </li>
       </ul>
 
+      <ul class="nav navbar-nav category">
+        <?php if($currentCategory): ?>
+          <li class="
+            <?php if ($url == "/index.php" . $urlPart): ?>
+              active
+            <?php endif; ?>
+          ">
+            <a href="/index.php<?= $urlPart?>"><?= $currentCategory ?></a>
+          </li>
+        <?php endif; ?>
+      </ul>
+
       <ul class="nav navbar-nav navbar-right">
         <?php if ($userService->isLoggedIn()): ?>
           <?php if ($userService->isAdmin()): ?>
-            <li>
-              <a href="/views/createPost.php" class="
+            <li class="
               <?php if ($url == '/views/createPost.php'): ?>
-                  active
+                active
               <?php endif; ?>
-              ">Create post</a>
+            ">
+              <a href="/views/createPost.php">Create post</a>
             </li>
           <?php endif; ?>
 
@@ -65,20 +101,20 @@ $url = substr($_SERVER['REQUEST_URI'], 0, strpos($_SERVER['REQUEST_URI'], "?"));
             </a>
           </li>
         <?php else: ?>
-          <li>
-            <a href="/views/logIn.php" class="
-              <?php if ($_SERVER['REQUEST_URI'] == '/views/logIn.php'): ?>
-                  active
-              <?php endif; ?>
-              ">Log In</a>
+          <li class="
+            <?php if ($_SERVER['REQUEST_URI'] == '/views/logIn.php'): ?>
+              active
+            <?php endif; ?>
+          ">
+            <a href="/views/logIn.php">Log In</a>
           </li>
 
-          <li>
-            <a href="../views/registration.php" class="
+          <li class="
               <?php if ($_SERVER['REQUEST_URI'] == '/views/registration.php'): ?>
-                  active
+                active
               <?php endif; ?>
-              ">Register</a>
+              ">
+            <a href="../views/registration.php">Register</a>
           </li>
         <?php endif; ?>
       </ul>
