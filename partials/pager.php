f725fc194e5e36a3919f57fc9fc57adc887ffebc
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-02-09 15:02:34 +0200

    Fix bug with pagination on categories pages.

--- partials/pager.php
+++ partials/pager.php
@@ -1,24 +1,31 @@
 <?php
 $urlPart = "";
+$urlPartForCheck = "";
+
 if (isset($_GET['category'])) {
-  $urlPart = "&category={$_GET['category']}";
+  $category = $_GET['category'];
+
+  $urlPart = "category={$category}&";
+  $urlPartForCheck = "?category={$category}";
 }
 
 $postsNumber = $postService->postCount();
+$postsNumber = ceil($postsNumber / 10) * 10;
 
 $offset = isset($_GET['offset']) ? (int) $_GET['offset'] : 0;
 
 $url = $_SERVER['REQUEST_URI'];
+$checkUrl = "/index.php" . $urlPartForCheck;
 ?>
 
 <nav class="text-center">
   <ul class="pagination">
     <li>
-      <?php if ($url === '/index.php' || $offset === 0): ?>
-        <a href="index.php?offset=<?= $postsNumber - 10 . $urlPart ?>">
+      <?php if ($url === $checkUrl || $url === $checkUrl . "&offset=0" || $url === $checkUrl . "?offset=0"): ?>
+        <a href="index.php?<?= $urlPart ?>offset=<?= $postsNumber - 10 ?>">
 
       <?php elseif ($offset): ?>
-        <a href="index.php?offset=<?= $offset - 10 . $urlPart ?>">
+        <a href="index.php?<?= $urlPart ?>offset=<?= $offset - 10 ?>">
 
       <?php endif; ?>
           <span>&laquo;</span>
@@ -48,7 +55,7 @@ $url = $_SERVER['REQUEST_URI'];
             class="hidden"
           <?php endif; ?>
 
-        <?php elseif ($url === '/index.php' || $url === '/index.php?offset=0'): ?>
+        <?php elseif ($url === $checkUrl || $url === $checkUrl . "&offset=0" || $url === $checkUrl . "?offset=0"): ?>
           <?php if ($i === 0): ?>
             class="active"
           <?php endif; ?>
@@ -59,7 +66,7 @@ $url = $_SERVER['REQUEST_URI'];
 
         <?php endif; ?>
       >
-        <a href="index.php?offset=<?= $i . $urlPart ?>">
+        <a href="index.php?<?= $urlPart ?>offset=<?= $i ?>">
           <?= $i / 10 + 1 ?>
         </a>
       </li>
@@ -67,16 +74,16 @@ $url = $_SERVER['REQUEST_URI'];
 
     <li>
       <?php if ($offset): ?>
-        <?php if ($offset === $postsNumber - 10): ?>
-          <a href="index.php?offset=0<?= $urlPart ?>">
+        <?php if ($offset == $postsNumber - 10): ?>
+          <a href="index.php?<?= $urlPart ?>offset=0">
 
         <?php else: ?>
-          <a href="index.php?offset=<?= $offset + 10 . $urlPart ?>">
+          <a href="index.php?<?= $urlPart ?>offset=0<?= $offset + 10 ?>">
 
         <?php endif; ?>
 
-      <?php elseif ($url === '/index.php' || $url === '/index.php?offset=0'): ?>
-        <a href=" index.php?offset=10<?= $urlPart ?>">
+      <?php elseif ($url === $checkUrl || $url === $checkUrl . "&offset=0" || $url === $checkUrl . "?offset=0"): ?>
+        <a href="index.php?<?= $urlPart ?>offset=10">
 
       <?php endif; ?>
           <span>&raquo;</span>
