6b9c6eafafd48d956ec9bbd3e7bfee613a4153ef
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-02-10 03:02:57 +0200

    Fix bug with user access to create post page.

--- views/createPost.php
+++ views/createPost.php
@@ -19,102 +19,109 @@ $categories = $categoryService->getAllCategories();
 <?php include realpath($_SERVER["DOCUMENT_ROOT"]) . '/partials/nav.php'; ?>
 
 <main class="container create-post">
-  <div class="text-center">
-    <h1>Create post</h1>
-  </div>
-
-  <div class="row alert alert-danger text-center hidden"
-       role="alert"
-       id="errors">
-  </div>
-
-  <form onsubmit="return ajaxSubmitCreatePostForm(this, <?= $postService->getLastPostId() ?>)"
-        method="post"
-        id="create_post_form"
-        enctype="multipart/form-data"
-        class="row well">
-
-    <div class="form-group">
-      <label for="title"
-             class="control-label">
-        Title
-      </label>
-      <input type="text"
-             name="title"
-             id="title"
-             placeholder="Enter title"
-             class="form-control"
-             required>
+  <?php if ($userService->isAdmin()): ?>
+    <div class="text-center">
+      <h1>Create post</h1>
     </div>
 
-    <div class="form-group">
-      <label for="image"
-             class="control-label">
-        Image
-      </label>
-      <input type="file"
-             name="image"
-             id="image"
-             accept="image/*"
-             class="form-control">
+    <div class="row alert alert-danger text-center hidden"
+         role="alert"
+         id="errors">
     </div>
 
-    <div class="form-group">
-      <label for="category"
-             class="control-label">
-        Category
-      </label>
-      <select name="category"
-              id="category"
-              class="form-control">
-        <option value="" disabled selected>- Select one -</option>
-        <?php foreach ($categories as $key => $value): ?>
-          <option value="<?= $key + 1 ?>">
-            <?= $value['category'] ?>
-          </option>
-        <?php endforeach; ?>
-      </select>
+    <form onsubmit="return ajaxSubmitCreatePostForm(this, <?= $postService->getLastPostId() ?>)"
+          method="post"
+          id="create_post_form"
+          enctype="multipart/form-data"
+          class="row well">
+
+      <div class="form-group">
+        <label for="title"
+               class="control-label">
+          Title
+        </label>
+        <input type="text"
+               name="title"
+               id="title"
+               placeholder="Enter title"
+               class="form-control"
+               required>
+      </div>
+
+      <div class="form-group">
+        <label for="image"
+               class="control-label">
+          Image
+        </label>
+        <input type="file"
+               name="image"
+               id="image"
+               accept="image/*"
+               class="form-control">
+      </div>
+
+      <div class="form-group">
+        <label for="category"
+               class="control-label">
+          Category
+        </label>
+        <select name="category"
+                id="category"
+                class="form-control">
+          <option value="" disabled selected>- Select one -</option>
+          <?php foreach ($categories as $key => $value): ?>
+            <option value="<?= $key + 1 ?>">
+              <?= $value['category'] ?>
+            </option>
+          <?php endforeach; ?>
+        </select>
+      </div>
+
+      <div class="form-group">
+        <label for="content"
+               class="control-label">
+          Content
+        </label>
+        <textarea class="ckeditor"
+                  id="content"
+                  name="content"
+                  class="form-control"
+                  required></textarea>
+        <script>
+          CKEDITOR.replace('content');
+        </script>
+      </div>
+
+      <div class="form-group">
+        <label for="teaser"
+               class="control-label">
+          Teaser
+        </label>
+        <textarea id="teaser"
+                  name="teaser"
+                  class="form-control"
+                  required></textarea>
+      </div>
+
+      <div class="form-group text-center">
+        <button type="submit"
+                class="btn btn-success">
+          Create
+        </button>
+
+        <a href="../index.php"
+           class="btn btn-danger">
+          Cancel
+        </a>
+      </div>
+    </form>
+  <?php else: ?>
+    <div class="row alert alert-danger text-center"
+         role="alert"
+         id="errors">
+      <p>You do not have access to view this page.</p>
     </div>
-
-    <div class="form-group">
-      <label for="content"
-             class="control-label">
-        Content
-      </label>
-      <textarea class="ckeditor"
-                id="content"
-                name="content"
-                class="form-control"
-                required></textarea>
-      <script>
-        CKEDITOR.replace('content');
-      </script>
-    </div>
-
-    <div class="form-group">
-      <label for="teaser"
-             class="control-label">
-        Teaser
-      </label>
-      <textarea id="teaser"
-                name="teaser"
-                class="form-control"
-                required></textarea>
-    </div>
-
-    <div class="form-group text-center">
-      <button type="submit"
-              class="btn btn-success">
-        Create
-      </button>
-
-      <a href="../index.php"
-         class="btn btn-danger">
-        Cancel
-      </a>
-    </div>
-
-  </form>
+  <?php endif; ?>
 </main>
 </body>
 </html>
