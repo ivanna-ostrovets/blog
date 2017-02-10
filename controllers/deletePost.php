dbd0c5f86cbc250ab8e1319f2878e37bc6aac8f7
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2017-02-10 02:58:03 +0200

    Fix bug on redirecting after deleting post.

--- controllers/deletePost.php
+++ controllers/deletePost.php
@@ -3,21 +3,29 @@ require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/services/postService.php');
 
 $postService->deletePost($_GET['id']);
 
-$urlParam = "?";
+$urlParam = "";
 $postsNumber = $postService->postCount();
+$offset = "";
+$category = "";
 
 if (isset($_GET['offset'])) {
-  $offset = (int) $_GET['offset'];
+  $offset = $_GET['offset'];
 
   if ($postsNumber == $offset) {
-    $newOffset = $offset - 10;
-    $urlParam .= "offset={$newOffset}&";
-  } else {
-    $urlParam .= "offset={$offset}&";
+    $offset = (int) $offset - 10;
   }
 }
 if (isset($_GET['category'])) {
-  $urlParam .= "category={$_GET['category']}";
+  $category = $_GET['category'];
+}
+
+if (!empty($offset) && !empty($category)) {
+  $urlParam .= "?offset={$offset}&category={$category}";
+} else if (!empty($offset)) {
+  $urlParam .= "?offset={$offset}";
+}
+else if (!empty($category)) {
+  $urlParam .= "?category={$category}";
 }
 
 header("Location: ../index.php{$urlParam}");
